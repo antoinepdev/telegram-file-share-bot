0250d6ce5eb5161e44924e0f3d623de4aca81e63
antoinepdev <antoinepdeveloper@gmail.com>
2026-02-20 19:43:19 -0500

    chore(env): config quick advertisement env variables

--- config.js
+++ config.js
@@ -5,18 +5,22 @@ function envVariablesValidator(envVariableKey, envVariableValue) {
   if (!envVariableValue)
     throw new Error(
       "Envarioment variable not provided: " +
-        envVariableKey +
-        " : " +
-        envVariableValue,
+      envVariableKey +
+      " : " +
+      envVariableValue,
     )
 }
 const botToken = process.env.BOT_TOKEN
 const storageGroupId = process.env.STORAGE_GROUP_ID
 const botUrl = process.env.BOT_URL
+const skipQuickAdGuideMessage = process.env.SKIP_QUICK_AD_GUIDE_MESSAGE
+const quickAdUrl = process.env.QUICK_AD_URL
 envVariablesValidator("BOT_TOKEN", botToken)
 envVariablesValidator("STORAGE_GROUP_ID", storageGroupId)
 envVariablesValidator("BOT_URL", botUrl)
+envVariablesValidator("SKIP_QUICK_AD_GUIDE_MESSAGE", skipQuickAdGuideMessage)
+envVariablesValidator("QUICK_AD_URL", quickAdUrl)
 
 const bot = new TelegramBot(botToken, { polling: true })
 
-export { bot, storageGroupId, botUrl }
+export { bot, storageGroupId, botUrl, skipQuickAdGuideMessage, quickAdUrl }
